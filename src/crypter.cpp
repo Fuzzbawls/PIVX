58d804540193d88dfab20a480fe5b400027f2bdb
furszy <matiasfurszyfer@protonmail.com>
2020-03-25 20:38:34 -0300

    [Sapling] Add Sapling keys to CCryptoKeyStore::EncryptKeys.
    Coming from 85beb9c9ca30a89b0c1aea973a4fd2f1ce16d38a .

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -229,6 +229,9 @@ bool CCryptoKeyStore::EncryptKeys(CKeyingMaterial& vMasterKeyIn)
                 return false;
         }
         mapKeys.clear();
+
+        // Now Sapling keys
+        return EncryptSaplingKeys(vMasterKeyIn);
     }
     return true;
 }
