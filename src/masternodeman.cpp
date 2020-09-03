292b2405a92b1ac48a213bce49415a6dc0b52768
Fuzzbawls <fuzzbawls@gmail.com>
2020-09-03 00:14:22 -0700

    fixup

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -17,6 +17,7 @@
 #include "swifttx.h"
 #include "util.h"
 
+#include <boost/thread/thread.hpp>
 
 #define MN_WINNER_MINIMUM_AGE 8000    // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
 
